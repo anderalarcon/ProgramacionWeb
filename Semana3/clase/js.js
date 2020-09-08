3670a07ae87ea343735fd385a329c6479dd2065b
anderalarcon <20172991@aloe.ulima.edu.pe>
2020-09-07 20:00:03 -0500

    Semana 3 sesion 1

---
+++ Semana3/clase/js.js
@@ -0,0 +1,33 @@
+document.write("Hola");
+console.log("Hola");
+
+//Variables
+var numero=100;
+numero="Billy";
+numero=true;
+listas=[1,2,3];
+numero=null;// null es un tipo de dato es jss
+
+var letra=undefined;
+console.log(letra);
+document.write(numero);
+console.log(numero);
+var a =1;
+let b=2;//no debe cambiar
+const c=3;
+document.write(a);
+document.write(b);
+
+var persona={//objeto como diccionarios se hace 
+
+    "Nombre":"ander",
+    "edad":15
+};
+persona.edad=36;
+console.log(persona.edad);
+
+function sumar(arg1,arg2){
+    return arg1+arg2;
+}
+
+console.log(sumar(1,2));
---
+++ Semana3/clase/js2.js
@@ -0,0 +1,12 @@
+
+
+function mostrarlog(){
+    var input=document.getElementById("nombre")
+    var boton=document.getElementById("boton");
+    var nombre=input.value;
+    boton.className="btn btn-success"
+    console.log(nombre);
+   
+}
+
+
