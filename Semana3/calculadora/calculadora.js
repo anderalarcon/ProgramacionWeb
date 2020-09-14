b85960675648770693954cf69f5b4d5633a57a3c
anderalarcon <20172991@aloe.ulima.edu.pe>
2020-09-14 14:27:49 -0500

    Ya entendi

--- Semana3/calculadora/calculadora.js
+++ Semana3/calculadora/calculadora.js
@@ -1,89 +1,89 @@
+var enOperacion = "";
+var numeroAlmacenado = 0;
+var borrar = false;
 
-var enOperacion="";
-var numeroAlmacenado=0;
-var borrar=false;
-
-function botonDigitoOnClick(evt){//al hacer click va realizar esta funcion en html 
+function botonDigitoOnClick(evt) { //recibe como parametro un evento . Y con ese evento obtengo donde se relizo el evento
     // digito es un int y esta definido ya en html ; numero es un string 
-    var digito=parseInt( evt.target.innerHTML);
-    var inputNumeros=document.getElementById("numeros");//chapamos x id 
+    var digito = parseInt(evt.target.innerHTML);
+    var inputNumeros = document.getElementById("numeros"); //chapamos x id 
 
-    var numero=inputNumeros.value;//el valor que esta en el input auxiliar  y para almacenar cacda vez 
-    if(numero=="0" || borrar==true){
-        inputNumeros.value=digito;//para que borre el 0 inicial
-        borrar=false;
-    }else{
-        inputNumeros.value=numero+digito;//int + string se concatena sin problemas 
+    var numero = inputNumeros.value; //el valor que esta en el input auxiliar  y para almacenar cacda vez 
+    if (numero == "0" || borrar == true) { //como es true y apretare un digito chanca
+        inputNumeros.value = digito; //para que borre el 0 inicial
+        borrar = false; //para que al apretar + y un digito diferente se borre el anterior y ponga el apretado
+    } else {
+        inputNumeros.value = numero + digito; //int + string se concatena sin problemas 
 
     }
 };
 
 
-var botonOperacionOnClick=function(){
-    var operacion=this.innerHTML//referencia al objeto al cual a sucedido el evento
-    if(operacion=="+"){
-        var inputNumeros=document.getElementById("numeros");
-        var numero=inputNumeros.value;
-        if(enOperacion==""){
-            enOperacion="+";
-            numeroAlmacenado=parseInt(numero);//numero almacenado es un int y numero es string
-        }else{
-           var resultado= numeroAlmacenado+parseInt(numero);
-           inputNumeros.value=resultado;
+var botonOperacionOnClick = function() { //otra manera de definir pero es lo misma mierda 
+    var operacion = this.innerHTML //referencia al objeto al cual a sucedido el evento osea a los + - * /
+    if (operacion == "+") {
+        var inputNumeros = document.getElementById("numeros");
+        var numero = inputNumeros.value;
+        if (enOperacion == "") {
+            enOperacion = "+";
+            numeroAlmacenado = parseInt(numero); //numero almacenado es un int y numero es string
+        } else {
+            var resultado = numeroAlmacenado + parseInt(numero);
+            inputNumeros.value = resultado;
 
         }
-        borrar=true;
-    }else if(operacion=="=") {
-        var inputNumeros=document.getElementById("numeros");
-        var numero=inputNumeros.value;
-        var resultado= numeroAlmacenado+parseInt(numero);
-        inputNumeros.value=resultado;
-        enOperacion="";
-        numeroAlmacenado=0;
-        borrar=true;
-    }else if (operacion=="C"){
-        var inputNumeros=document.getElementById("numeros");
-        var numero=inputNumeros.value;
-        inputNumeros.value=0
-        enOperacion="";
-        numeroAlmacenado=0;
-        borrar=false;
-        numero="0";
+        borrar = true;
+    } else if (operacion == "=") {
+        var inputNumeros = document.getElementById("numeros");
+        var numero = inputNumeros.value;
+        var resultado = numeroAlmacenado + parseInt(numero); //numero almacenado xq antes de dar igual tendria que darle click a alguna operacion
+        inputNumeros.value = resultado;
+        enOperacion = "";
+        numeroAlmacenado = 0;
+        borrar = true;
+
+    } else if (operacion == "C") {
+        var inputNumeros = document.getElementById("numeros");
+        var numero = inputNumeros.value;
+        inputNumeros.value = 0;
+        enOperacion = "";
+        numeroAlmacenado = 0;
+        borrar = false;
+        numero = "0";
+
+    } else if (operacion == "*") {
+        var inputNumeros = document.getElementById("numeros");
+        var numero = inputNumeros.value;
+        if (enOperacion == " ") {
+            enOperacion = "*";
+            numeroAlmacenado = parseInt(numero);
+        } else {
+            var resultado = numeroAlmacenado * parseInt(numero);
+            inputNumeros.value = resultado;
+
+        }
+        borrar = true;
+
+
+
     }
-};
+}
 
 
-
-//hola
-/*var main=function(){
-    for(var i =0;i<10;i++){
-        document.getElementById("bot"+i).onclick=botonDigitoOnClick;
-    }//no esta ejecutando ,le esta asignando al atributo onclick. Recien se ejecta cuando alguien le da click al boton
-    
-};*/
-
-//window.onload=main;//asi lo hace el profe para que al cargar ya este todo ok
-
-var main=function(){//otra forma de realizarlo
+var main = function() { //para funciones concatenadas
     //configurando botones de digito
-    for(var i =0;i<10;i++){
-        var but=document.getElementById("bot"+i);
-        but.addEventListener("click",botonDigitoOnClick);
-        but.addEventListener("click",function(){
-            console.log("Se hizo click en boton");
-            console.log(this);
-        });
+    for (var i = 0; i < 10; i++) {
+        var but = document.getElementById("bot" + i);
+        but.addEventListener("click", botonDigitoOnClick); //"clicl al hacer click"
+
     }
+
     //configurando botones de operaciones
 
-    document.getElementById("botsuma").addEventListener("click",botonOperacionOnClick);
-    document.getElementById("botigual").addEventListener("click",botonOperacionOnClick);
-    document.getElementById("botonC").addEventListener("click",botonOperacionOnClick);
+    document.getElementById("botsuma").addEventListener("click", botonOperacionOnClick);
+    document.getElementById("botigual").addEventListener("click", botonOperacionOnClick);
+    document.getElementById("botonC").addEventListener("click", botonOperacionOnClick);
+    document.getElementById("boton*").addEventListener("click", botonOperacionOnClick);
 
-
-
-
-    
 };
 
-window.onload=main;
+window.addEventListener("load", main); //cuando cargue la ventana se va a ejecutar la funcion main
