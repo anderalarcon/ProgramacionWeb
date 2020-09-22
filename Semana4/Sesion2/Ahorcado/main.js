be606b16c2cba977ed2638f95c7a8c5b040a7641
anderalarcon <20172991@aloe.ulima.edu.pe>
2020-09-21 20:45:22 -0500

    Semana5Sesion1

---
+++ Semana4/Sesion2/Ahorcado/main.js
@@ -0,0 +1,133 @@
+var FRASES=[
+    "AL QUE MADRUGA DIOS LE AYUDA",
+    "A CABALLO REGALADO NO SE LE MIRA EL DIENTE"
+];
+
+var refranSeleccionado=null;
+var refranOculto=null;
+var contadorErrores=0;
+
+var ocultar=function(refran){//hacemos las lineas
+    var refranoculto="";
+    for (var i of refran){//Bota las letras altok ; x cada letra
+        if(i !=" "){
+            refranoculto=refranoculto+"_";
+        }else{
+            refranoculto=refranoculto+"  ";
+        }
+    }
+    return refranoculto;
+};
+
+var mostrarrefranoculto=function(refran){
+   // document.querySelector("#refran").innerHTML=refran; con #
+    document.getElementById("refran").innerHTML=refran;
+}
+
+
+var verificarcaracterenrefran=function(caracter,refran){
+            for(var c of refran){
+                if(c==caracter){
+                    return true;
+                }
+            }
+            return false;//si termina de recorrer y no encuentra
+}
+
+
+
+
+var descubrircaracterenrefran=function(c,refranselec,refranocul){
+        var refran="";
+        for(var i in refranselec){
+            if(c==refranselec[i]){
+                refran+=c;
+            }else{
+                refran+=refranocul[i]
+            }
+        }
+        refranocul=refran;
+        mostrarrefranoculto(refranOculto);
+}
+
+var verificarganador=function(){
+    return refranOculto==refranSeleccionado;//devuelve true o false
+}
+
+
+var mostrarmensaje=function(){
+    document.getElementById("msjganador").className="visible";//clase en css
+    document.getElementById("msjperdedor").className="oculto";
+};
+
+var actualizarImagen=function(){
+    document.getElementById("imagen").setAttribute("src","imagenes/Hangman-" 
+    +  ++contadorErrores +".png")
+
+    //++a -> antes aumenta  a++ -> ejecuta y luego aumenta 1
+}
+
+
+var verificarperdedor=function(){
+
+    return contadorErrores>=6;//boleano si se cumple devuevle true ,sino false
+}
+
+var mostrarmsjperdedor=function(){
+    document.getElementById("msjganador").className="oculto";//clase en css
+    document.getElementById("msjperdedor").className="visible";
+}
+
+var teclaapretada=function(evt){
+    var caracter=evt.key;//almacena las letras
+
+    verificarcaracterenrefran(caracter,refranSeleccionado);
+
+    if(verificarcaracterenrefran){
+        
+        descubrircaracterenrefran(caracter,refranSeleccionado,refranOculto);
+
+        var hayGanador=verificarganador();
+
+        if(hayGanador){
+            mostrarmensaje();
+        }
+    }else{
+        actualizarImagen();
+        var perdio=verificarperdedor();
+        if(perdio){
+            mostrarmsjperdedor();
+        }
+       
+}
+}
+
+var setearlistener=function(){
+    document.getElementById("caracteres_repetidos").addEventListener("keypress",teclaapretada)
+}
+
+
+var main=function(){
+    /*
+    1.Elegir un refran
+    2.Ocultar el refran
+    3.Setear un listener que reaccione al presionado de una tecla
+        3.1Verificar que la letra presionada esta dentro del refran
+        3.2Caso en que si este : Descubrir letra seleccionada en refran
+            3.2.1 Si hay ganador : Descubrir msj correcto
+        3.3Caso que no este : Actualizar la imagen y el contador de errores
+            3.3.1 Verificar si perdio : Mostrar msj
+        3.4Concatenar letra en caja de texto
+
+
+    */
+
+    refranSeleccionado=FRASES[0];
+    refranOculto=ocultar(refranSeleccionado);
+    mostrarrefranoculto(refranOculto);
+    setearlistener();
+    
+    
+};
+
+window.addEventListener("load",main);
