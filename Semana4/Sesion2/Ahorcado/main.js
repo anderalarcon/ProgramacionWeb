554492baef362126924ef5dddb969004f3a74865
anderalarcon <20172991@aloe.ulima.edu.pe>
2020-09-23 02:08:06 -0500

    Ahorcado corregido

--- Semana4/Sesion2/Ahorcado/main.js
+++ Semana4/Sesion2/Ahorcado/main.js
@@ -1,114 +1,113 @@
-var FRASES=[
-    "AL QUE MADRUGA DIOS LE AYUDA",
-    "A CABALLO REGALADO NO SE LE MIRA EL DIENTE"
+var FRASES = [
+  "AL QUE MADRUGA DIOS LE AYUDA",
+  "A CABALLO REGALADO NO SE LE MIRA EL DIENTE",
 ];
 
-var refranSeleccionado=null;
-var refranOculto=null;
-var contadorErrores=0;
+var refranSeleccionado = null;
+var refranOculto = null;
+var contadorErrores = 0;
 
-var ocultar=function(refran){//hacemos las lineas
-    var refranoculto="";
-    for (var i of refran){//Bota las letras altok ; x cada letra
-        if(i !=" "){
-            refranoculto=refranoculto+"_";
-        }else{
-            refranoculto=refranoculto+"  ";
-        }
+var ocultar = function (refran) {
+  //hacemos las lineas
+  var refranoculto = "";
+  for (var i of refran) {
+    //Bota las letras altok ; x cada letra
+    if (i != " ") {
+      refranoculto += "_";
+    } else {
+      refranoculto = refranoculto + " ";
     }
-    return refranoculto;
+  }
+  return refranoculto;
 };
 
-var mostrarrefranoculto=function(refran){
-   // document.querySelector("#refran").innerHTML=refran; con #
-    document.getElementById("refran").innerHTML=refran;
-}
-
-
-var verificarcaracterenrefran=function(caracter,refran){
-            for(var c of refran){
-                if(c==caracter){
-                    return true;
-                }
-            }
-            return false;//si termina de recorrer y no encuentra
-}
-
-
-
-
-var descubrircaracterenrefran=function(c,refranselec,refranocul){
-        var refran="";
-        for(var i in refranselec){
-            if(c==refranselec[i]){
-                refran+=c;
-            }else{
-                refran+=refranocul[i]
-            }
-        }
-        refranocul=refran;
-        mostrarrefranoculto(refranOculto);
-}
-
-var verificarganador=function(){
-    return refranOculto==refranSeleccionado;//devuelve true o false
-}
-
-
-var mostrarmensaje=function(){
-    document.getElementById("msjganador").className="visible";//clase en css
-    document.getElementById("msjperdedor").className="oculto";
+var mostrarrefranoculto = function (refran) {
+  // document.querySelector("#refran").innerHTML=refran; con #
+  document.querySelector("#refran").innerHTML = refran;
 };
 
-var actualizarImagen=function(){
-    document.getElementById("imagen").setAttribute("src","imagenes/Hangman-" 
-    +  ++contadorErrores +".png")
+var verificarcaracterenrefran = function (caracter, refran) {
+  for (var c of refran) {
+    if (c == caracter) {
+      return true;
+    }
+  }
+  return false; //si termina de recorrer y no encuentra
+};
 
-    //++a -> antes aumenta  a++ -> ejecuta y luego aumenta 1
-}
+var descubrircaracterenrefran = function (c, refranselec, refranocul) {
+  var refran = "";
+  for (var i in refranselec) {
+    if (c == refranselec[i]) {
+      refran += c;
+    } else {
+      refran += refranocul[i];
+    }
+  }
+  return refran;
+};
 
+var verificarganador = function () {
+  return refranOculto == refranSeleccionado; //devuelve true o false
+};
 
-var verificarperdedor=function(){
+var mostrarmensaje = function () {
+  //ganador
+  document.getElementById("msjganador").className = "visible"; //clase en css
+  document.getElementById("msjperdedor").className = "oculto";
+};
 
-    return contadorErrores>=6;//boleano si se cumple devuevle true ,sino false
-}
+var actualizarImagen = function () {
+  document
+    .querySelector("#imagen")
+    .setAttribute("src", "imagenes/Hangman-" + ++contadorErrores + ".png");
 
-var mostrarmsjperdedor=function(){
-    document.getElementById("msjganador").className="oculto";//clase en css
-    document.getElementById("msjperdedor").className="visible";
-}
+  //++a -> antes aumenta  a++ -> ejecuta y luego aumenta 1
+};
 
-var teclaapretada=function(evt){
-    var caracter=evt.key;//almacena las letras
+var verificarperdedor = function () {
+  return contadorErrores >= 6; //boleano si se cumple devuevle true ,sino false
+};
 
-    verificarcaracterenrefran(caracter,refranSeleccionado);
+var mostrarmsjperdedor = function () {
+  document.getElementById("msjganador").className = "oculto"; //clase en css
+  document.getElementById("msjperdedor").className = "visible";
+};
 
-    if(verificarcaracterenrefran){
-        
-        descubrircaracterenrefran(caracter,refranSeleccionado,refranOculto);
+var teclaapretada = function (evt) {
+  var caracter = evt.key.toUpperCase(); //almacena las letras
 
-        var hayGanador=verificarganador();
+  var estaEnrefran = verificarcaracterenrefran(caracter, refranSeleccionado);
 
-        if(hayGanador){
-            mostrarmensaje();
-        }
-    }else{
-        actualizarImagen();
-        var perdio=verificarperdedor();
-        if(perdio){
-            mostrarmsjperdedor();
-        }
-       
-}
-}
+  if (estaEnrefran) {
+    refranOculto = descubrircaracterenrefran(
+      caracter,
+      refranSeleccionado,
+      refranOculto
+    );
+    mostrarrefranoculto(refranOculto);
+    var hayGanador = verificarganador();
 
-var setearlistener=function(){
-    document.getElementById("caracteres_repetidos").addEventListener("keypress",teclaapretada)
-}
+    if (hayGanador) {
+      mostrarmensaje();
+    }
+  } else {
+    actualizarImagen();
+    var perdio = verificarperdedor();
+    if (perdio) {
+      mostrarmsjperdedor();
+    }
+  }
+};
 
+var setearlistener = function () {
+  document
+    .querySelector("#caracteres_repetidos")
+    .addEventListener("keypress", teclaapretada);
+};
 
-var main=function(){
-    /*
+var main = function () {
+  /*
     1.Elegir un refran
     2.Ocultar el refran
     3.Setear un listener que reaccione al presionado de una tecla
@@ -122,12 +121,10 @@ var main=function(){
 
     */
 
-    refranSeleccionado=FRASES[0];
-    refranOculto=ocultar(refranSeleccionado);
-    mostrarrefranoculto(refranOculto);
-    setearlistener();
-    
-    
+  refranSeleccionado = FRASES[0];
+  refranOculto = ocultar(refranSeleccionado);
+  mostrarrefranoculto(refranOculto);
+  setearlistener();
 };
 
-window.addEventListener("load",main);
+window.addEventListener("load", main);
